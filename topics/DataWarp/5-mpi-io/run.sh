90d03cc810402033d8350c838c6873ba88d81a29
Mario Valle <mvalle@cscs.ch>
2017-07-18 16:29:19 +0200

    Run MPI I/O on datawarp

---
+++ topics/DataWarp/5-mpi-io/run.sh
@@ -0,0 +1,28 @@
+tmp=$SCRATCH/dwtest
+if [ ! -d $tmp ]
+then
+    mkdir $tmp
+fi
+cp a.out $tmp
+cd $tmp
+
+sbatch -C gpu <<\EOF
+#!/bin/bash
+#SBATCH --job-name="DW-MPI/IO"
+#SBATCH --nodes=3
+#SBATCH --time=0:05:00
+#SBATCH --output=mpi-io.out%j
+#DW jobdw access_mode=striped capacity=200GiB type=scratch
+#-----------------------------------------------------------
+
+export MPICH_MPIIO_HINTS="*out.dat:cb_nodes=8"
+
+export MPICH_MPIIO_HINTS_DISPLAY=1
+
+echo "================================================"
+srun -N 3 ./a.out
+echo "================================================"
+cat -v $DW_JOB_STRIPED/out.dat
+
+EOF
+
