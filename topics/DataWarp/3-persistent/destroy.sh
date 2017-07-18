6abfbbf8bc4be8ed80294dc2c345d4b986f679b3
Mario Valle <mvalle@cscs.ch>
2017-07-18 16:20:11 +0200

    Destroy permanent partition

---
+++ topics/DataWarp/3-persistent/destroy.sh
@@ -0,0 +1,12 @@
+
+cd $SCRATCH/dwtest
+
+sbatch -C mc <<\EOF
+#!/bin/bash
+#SBATCH --job-name="DW-destroy-permanent"
+#SBATCH --nodes=1
+#SBATCH --time=0:05:00
+#SBATCH --output=/dev/null
+#BB destroy_persistent name=mvalleBB
+EOF
+
